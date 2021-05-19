d626a4f4b17d8fdadfff5859d8367689f8946991
Nadia.Auto <nadiia.tyshko@fortegrp.com>
2021-05-19 19:31:43 +0300

    Section 5

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -9,9 +9,14 @@ const HomeScreen = ({ navigation }) => {
               onPress={() => navigation.navigate('Components')}
               title="Go to Components Demo"
             />
-            <TouchableOpacity onPress={() => navigation.navigate('List')}>
-              <Text>Go to List Demo</Text>
-            </TouchableOpacity>
+            <Button
+              onPress={() => navigation.navigate('List')}
+              title="Go to List Demo"
+            />
+            <Button
+              onPress={() => navigation.navigate('Image')}
+              title="Go to Image Demo"
+            />
         </View>
     );
 };
